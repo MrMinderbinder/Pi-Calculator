192976355b9f3c0ea126700e35cf0eeaef263981
MrMinderbinder <54345084+MrMinderbinder@users.noreply.github.com>
2022-12-24 14:35:20 +1300

    Update pi_madhava.py
    Minor readability improvements

--- pi_madhava.py
+++ pi_madhava.py
@@ -1,17 +1,17 @@
 import decimal
 import time
 
-digits_selection = input("Enter number of digits: ")
+digits = input("Enter number of digits: ")
+overhead = 11
 
-decimal.getcontext().prec = int(digits_selection) + 11
+decimal.getcontext().prec = int(digits) + overhead
 
 start = time.time()
-
-total = 10 ** (int(digits_selection) + 10)
+total = 10 ** (int(digits) + overhead - 1)
 term = total
 k = 0
 
-while term: 
+while term:
     term = term // (-6 * k - 9) * (2 * k + 1)
     total += term
     k += 1
@@ -19,5 +19,5 @@ while term:
 pi = str(decimal.Decimal(12).sqrt() * total)
 end = time.time()
 
-print(pi[0] + "." + pi[1:-11])
+print(pi[0] + "." + pi[1:-overhead])
 print("Computation time: ", end - start, "s", sep = "")
